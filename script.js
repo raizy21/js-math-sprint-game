0f30bcb2c79fb3a36ca460f9d325763cf4b37c50
raizy21 <chiper_raizy@yahoo.com>
2021-01-11 08:41:55 +0100

    init_math-s[rint-game

---
+++ script.js
@@ -0,0 +1,10 @@
+/***************************
+ * links
+ * {@link} -  https://www.w3schools.com/howto/howto_css_hide_scrollbars.asp
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+ * {@link} - https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/API/Element/scroll
+ * {@link} - https://www.w3schools.com/js/js_timing.asp
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
+ *
+ ***************************/
